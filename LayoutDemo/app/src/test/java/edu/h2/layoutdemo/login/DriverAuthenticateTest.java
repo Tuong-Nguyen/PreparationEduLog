4d7cde65df4cacb076b2c8ae25b218a2b7f6ba4e
Hanh <ntmhanh@tma.com.vn>
2017-06-13 11:01:52 +0700

    - Add init() for before test

--- LayoutDemo/app/src/test/java/edu/h2/layoutdemo/login/DriverAuthenticateTest.java
+++ LayoutDemo/app/src/test/java/edu/h2/layoutdemo/login/DriverAuthenticateTest.java
@@ -1,6 +1,7 @@
 package edu.h2.layoutdemo.login;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -18,17 +19,24 @@ import static org.mockito.Mockito.when;
  */
 
 public class DriverAuthenticateTest {
+    private Driver driver;
+    private DriverAuthenticateUseCase driverAuthenticateUseCase;
+    private LoginPresenter.RequireLoginPresenterOptions requireLoginPresenterOptions;
+    String busId = "1";
+    String driverId = "2";
+    String password = "123";
+
+    @Before
+    public void init(){
+        driver = new Driver(busId, driverId, password);
+        requireLoginPresenterOptions = Mockito.mock(LoginPresenterImplement.class);
+        DriverRepository driverRepository = Mockito.mock(DriverRepository.class);
+        driverAuthenticateUseCase = new DriverAuthenticateUseCase(requireLoginPresenterOptions, driverRepository);
+    }
 
     @Test
     public void validateCredentials_InputDriverExistWithRightBusIdAndPassword_alertOnLoginSuccess(){
         // Arrange
-        String busId = "1";
-        String driverId = "2";
-        String password = "123";
-        Driver driver = new Driver("1","2", "123");
-        LoginPresenter.RequireLoginPresenterOptions requireLoginPresenterOptions = Mockito.mock(LoginPresenterImplement.class);
-        DriverRepository driverRepository = Mockito.mock(DriverRepository.class);
-        DriverAuthenticateUseCase driverAuthenticateUseCase = new DriverAuthenticateUseCase(requireLoginPresenterOptions, driverRepository);
         when(driverAuthenticateUseCase.mDriverRepository.getDriverById(Mockito.anyString())).thenReturn(driver);
         // Action
         driverAuthenticateUseCase.validateCredentials(busId, driverId, password);
@@ -38,13 +46,6 @@ public class DriverAuthenticateTest {
     @Test
     public void validateCredentials_InputDriverExistWithWrongBusIdOrPassword_alertOnLoginFail(){
         // Arrange
-        String busId = "1";
-        String driverId = "2";
-        String password = "123";
-        Driver driver = new Driver("1","2", "123");
-        LoginPresenter.RequireLoginPresenterOptions requireLoginPresenterOptions = Mockito.mock(LoginPresenterImplement.class);
-        DriverRepository driverRepository = Mockito.mock(DriverRepository.class);
-        DriverAuthenticateUseCase driverAuthenticateUseCase = new DriverAuthenticateUseCase(requireLoginPresenterOptions, driverRepository);
         when(driverAuthenticateUseCase.mDriverRepository.getDriverById(Mockito.anyString())).thenReturn(driver);
         // Action
         driverAuthenticateUseCase.validateCredentials("2", driverId, password);
@@ -54,13 +55,7 @@ public class DriverAuthenticateTest {
     @Test
     public void validateCredentials_InputDriverNotExist_returnFalse(){
         // Arrange
-        String busId = "1";
-        String driverId = "2";
-        String password = "123";
-        LoginPresenter.RequireLoginPresenterOptions requireLoginPresenterOptions = Mockito.mock(LoginPresenterImplement.class);
-        DriverRepository driverRepository = Mockito.mock(DriverRepository.class);
-        DriverAuthenticateUseCase driverAuthenticateUseCase = new DriverAuthenticateUseCase(requireLoginPresenterOptions, driverRepository);
-        when(driverAuthenticateUseCase.mDriverRepository.getDriverById(Mockito.anyString())).thenReturn(null);
+               when(driverAuthenticateUseCase.mDriverRepository.getDriverById(Mockito.anyString())).thenReturn(null);
         // Action
         boolean isLogin = driverAuthenticateUseCase.validateCredentials(busId, driverId, password);
         //Assert
