6918cd8f1cf7aa9a073680a83b936e9f98977580
Tuong Nguyen <wallnguyen@gmail.com>
2017-06-19 17:53:58 +0700

    Update variable name

--- DriverPortal/app/src/test/java/com/edulog/driverportal/login/DriverAuthenticateUseCaseTest.java
+++ DriverPortal/app/src/test/java/com/edulog/driverportal/login/DriverAuthenticateUseCaseTest.java
@@ -5,14 +5,11 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.edulog.driverportal.login.domain.services.AuthenticateService;
-import com.edulog.driverportal.login.domain.services.AuthenticateServiceImplement;
 import com.edulog.driverportal.login.domain.interactors.DriverAuthenticateUseCase;
 
 import io.reactivex.Observable;
-import io.reactivex.Scheduler;
 import io.reactivex.observers.TestObserver;
 import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subscribers.TestSubscriber;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
@@ -27,24 +24,24 @@ public class DriverAuthenticateUseCaseTest {
     String busId = "1";
     String driverId = "2";
     String password = "123";
-    AuthenticateService authenticateServiceImplement;
+    AuthenticateService authenticateService;
 
     @Before
     public void init() {
-        authenticateServiceImplement = Mockito.mock(AuthenticateService.class);
-        driverAuthenticateUseCase = new DriverAuthenticateUseCase(Schedulers.trampoline(), authenticateServiceImplement);
+        authenticateService = Mockito.mock(AuthenticateService.class);
+        driverAuthenticateUseCase = new DriverAuthenticateUseCase(Schedulers.trampoline(), authenticateService);
     }
 
     @Test
     public void execute_inputLoginInformation_returnAuthenticateWasCalled() {
         // Arrange
         DriverAuthenticateUseCase.Params params = new DriverAuthenticateUseCase.Params(busId, driverId, password);
-        when(authenticateServiceImplement.authenticate(anyString(), anyString())).thenReturn(Observable.just(true));
+        when(authenticateService.authenticate(anyString(), anyString())).thenReturn(Observable.just(true));
         TestObserver<Boolean> testObserver = new TestObserver<>();
         // Action
         driverAuthenticateUseCase.execute(testObserver, params);
         // Assert
-        verify(authenticateServiceImplement).authenticate(params.driverId, params.password);
+        verify(authenticateService).authenticate(params.driverId, params.password);
         testObserver.assertValue(true);
     }
 }
