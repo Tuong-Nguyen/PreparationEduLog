138ada681d4e20565f1c416dc53bb0e5ff31c82d
Le Ngoc Thao <lnthao@tma.com.vn>
2017-06-19 10:20:01 +0700

    Remove unnecessary project

--- LayoutDemo/app/src/main/java/edu/h2/layoutdemo/login/models/Events.java
+++
@@ -1,9 +0,0 @@
-package edu.h2.layoutdemo.login.models;
-
-/**
- * Define enum events to send request
- */
-
-public enum Events {
-    LOG_IN
-}
--- LayoutDemo/app/src/main/java/edu/h2/layoutdemo/presentation/settings/SettingsView.java
+++
@@ -1,6 +0,0 @@
-package edu.h2.layoutdemo.presentation.settings;
-
-import edu.h2.layoutdemo.presentation.BaseView;
-
-public interface SettingsView extends BaseView {
-}
